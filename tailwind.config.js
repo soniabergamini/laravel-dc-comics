a3d9a3a52764b618bd1e3635ded45dc647049bd1
soniabergamini <sounia96@outlook.it>
2023-07-14 21:20:28 +0200

    style: add tailwind alert style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
     content: [
         "./resources/**/*.blade.php",
@@ -14,5 +17,47 @@ module.exports = {
             }
         },
     },
-    plugins: []
+    plugins: [
+        plugin(function ({ addComponents }) {
+            addComponents({
+                '.alert': {
+                    position: 'relative',
+                    padding: '0.75rem 1.25rem',
+                    marginBottom: '1rem',
+                    border: '1px solid transparent',
+                    borderRadius: '0.25rem',
+                },
+                '.alert-danger': {
+                    color: '#721c24',
+                    backgroundColor: '#f8d7da',
+                    borderColor: '#f5c6cb'
+                },
+                '.invalid-feedback': {
+                    width: '100%',
+                    marginTop: '-.6rem',
+                    marginBottom: '.8rem',
+                    fontSize: '80%',
+                    color: '#dc3545'
+                },
+                '.is-invalid': {
+                    borderColor: '#dc3545'
+                },
+                '.form-control': {
+                    borderColor: '#dc3545',
+                    marginBottom: '1rem',
+                    display: 'block',
+                    width: '100%',
+                    padding: '0.375rem 0.75rem',
+                    fontSize: '1rem',
+                    lineHeight: '1.5',
+                    color: '#495057',
+                    backgroundColor: '#fff',
+                    backgroundClip: 'padding-box',
+                    border: '1px solid #ced4da',
+                    borderRadius: '0.25rem',
+                    transition: 'border-color .15s ease-in-out, box-shadow .15s ease-in-out'
+                },
+            })
+        })
+    ]
 }
